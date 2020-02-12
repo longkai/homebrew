565b2fc6648af941a7d150a7fd85d61904d6cbeb
longkai <im.longkai@gmail.com>
2020-02-12 14:56:18 +0800

    Add sketch legacy

---
+++ Casks/sketch-legacy.rb
@@ -0,0 +1,25 @@
+cask 'sketch-legacy' do
+  version '52.6-67491'
+  sha256 '459aceeeb00f12d996f6b24aa852b0712806a9f3c4d2830182b5f65ba51b3454'
+
+  url "https://download.sketchapp.com/sketch-#{version}.zip"
+  appcast 'https://download.sketchapp.com/sketch-versions.xml'
+  name 'Sketch'
+  homepage 'https://www.sketchapp.com/'
+
+  auto_updates true
+  depends_on macos: '>= :high_sierra'
+
+  app 'Sketch.app'
+
+  zap trash: [
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.sfl*',
+               '~/Library/Application Support/com.bohemiancoding.sketch3',
+               '~/Library/Caches/com.bohemiancoding.sketch3',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3',
+               '~/Library/Logs/com.bohemiancoding.sketch3',
+               '~/Library/Preferences/com.bohemiancoding.sketch3.LSSharedFileList.plist',
+               '~/Library/Preferences/com.bohemiancoding.sketch3.plist',
+               '~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies',
+             ]
+end
